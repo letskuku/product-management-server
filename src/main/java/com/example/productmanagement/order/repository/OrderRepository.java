d0e1fe8daa46d7d6086775aa150b9d1a665a5df1
letskuku <yujung9288@naver.com>
2023-11-07 02:39:04 +0900

    feat: repository 생성

---
+++ src/main/java/com/example/productmanagement/order/repository/OrderRepository.java
@@ -0,0 +1,9 @@
+package com.example.productmanagement.order.repository;
+
+import com.example.productmanagement.order.domain.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrderRepository extends JpaRepository<Order, Long> {
+}
---
+++ src/main/java/com/example/productmanagement/user/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.example.productmanagement.user.repository;
+
+import com.example.productmanagement.user.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
